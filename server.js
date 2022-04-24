ca9f21582a788016c19e17f1e43523f618ffc261
tarun.amasa@duke.edu <tarun.amasa@duke.edu>
2022-04-24 12:32:22 -0400

    tried to test with browser sync

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const fs = require('fs')
 const minimist = require('minimist')
 
 console.log("Hello world!")
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World\n')
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
